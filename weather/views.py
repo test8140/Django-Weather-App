ad0300649449273f1d4171c3fde5281485ce6774
test8140 <test8140@ukr.net>
2020-02-01 11:13:40 +0200

    Compiling a dictionary to obtain the necessary data, editing the output form on the screen, Creating a City model, migration, Administering the City table, Create a list to display data from the City table, display it visually on the page.

--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 import requests
 
+from .models import City
+
 
 def index(request):
     appid = 'a9d0ce4653a3d5bb5a14558470bb9082'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    return render(request, 'weather/index.html')
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main'] ['temp'],
+            'icon': res['weather'] [0] ['icon'] 
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities}
+
+    return render(request, 'weather/index.html', context)
