4f4b9fa94b7ddb45f2a5db38dfb04d226e9ba30d
test8140 <test8140@ukr.net>
2020-02-01 10:28:13 +0200

    Filling the page with bootstrap styles, editing, Connecting a key from the site, connecting requests.

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import requests
+
 
 def index(request):
+    appid = 'a9d0ce4653a3d5bb5a14558470bb9082'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
     return render(request, 'weather/index.html')
